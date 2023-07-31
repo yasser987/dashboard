cb34e1d574d6d5ab40f73b19b5a1571db5e561fb
Yasser <eng.yasserlebana2001@gmail.com>
2023-07-31 10:58:33 +0300

    modified

--- pages/dashboard/analytics/analytics.tsx
+++ pages/dashboard/analytics/analytics.tsx
@@ -1,30 +1,28 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { DataGridPro } from "@mui/x-data-grid-pro";
-import { useDemoData } from "@mui/x-data-grid-generator";
-import { useMediaQuery } from "@mui/material";
+//import { DataGridPro } from "@mui/x-data-grid-pro";
+//import { useDemoData } from "@mui/x-data-grid-generator";
+//import { useMediaQuery } from "@mui/material";
 
 export default function analytics() {
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 100000,
-    editable: true,
-  });
-
-  const isMobile = useMediaQuery("(max-width: 900px)");
+  //  const { data } = useDemoData({
+  //    dataSet: "Commodity",
+  //    rowLength: 100000,
+  //    editable: true,
+  //  });
+  //
+  //  const isMobile = useMediaQuery("(max-width: 900px)");
 
   return (
-    <Box
-      sx={{ height: 520, width: "75%", margin: "0 auto" }}
-      width={isMobile ? "90%" : "75%"}
-    >
-      <DataGridPro
+    <Box sx={{ height: 520, width: "75%", margin: "0 auto" }}>
+      {/*<DataGridPro
         {...data}
         loading={data.rows.length === 0}
         rowHeight={38}
         checkboxSelection
         disableRowSelectionOnClick
-      />
+      />*/}
+      <h1>Analytics</h1>
     </Box>
   );
 }
